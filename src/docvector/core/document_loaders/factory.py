b50abb576110df73250f7cad2c56fcef27cb58f3
Phillip Karamagi <pkaramagi1@gmail.com>
2025-03-06 21:49:02 +0200

    feat: add document factory

---
+++ src/docvector/core/document_loaders/factory.py
@@ -0,0 +1,15 @@
+from src.docvector.core.document_loaders.base import DocumentLoader
+from src.docvector.core.document_loaders.pdf_loader import PdfLoader
+from src.docvector.core.document_loaders.text_loader import TextLoader
+
+
+class DocumentLoaderFactory:
+    @staticmethod
+    def get_loader(path:str) -> DocumentLoader:
+        if path.endswith('txt'):
+            return TextLoader()
+        elif path.endswith('pdf'):
+            return PdfLoader()
+        raise ValueError("Unsupported File Format")
+    
+    
