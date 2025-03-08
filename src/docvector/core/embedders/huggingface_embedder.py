c4ae9367353704b1bdf1be3b09128abab86109cb
Phillip Karamagi <pkaramagi1@gmail.com>
2025-03-08 22:36:02 +0200

    feat:add huggingface embedder using sentence-transformers

---
+++ src/docvector/core/embedders/huggingface_embedder.py
@@ -0,0 +1,16 @@
+from typing import List
+
+from sentence_transformers import SentenceTransformer
+from core.embedders.base import Embedder
+
+
+class HuggingFaceEmbedder(Embedder):
+    def __init__(self, model_name:str = "sentence-transformers/all-MiniLM-L6-v2", device:str = None):
+        self.device = device or "cpu"
+        self.model = SentenceTransformer(model_name).to(self.device)
+
+    def embed(self, text:str) -> List[float]:
+        return self.model.encode(text, convert_to_nuppy=True).tolist()
+    
+    def embed_batch(self, texts:List[str]) -> List[List[float]]:
+        return self.model.encode(texts, convert_to_nuppy=True).tolist()
