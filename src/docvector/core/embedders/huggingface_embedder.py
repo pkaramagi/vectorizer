0558ec0554cf09e52fbb39ab25c5c4aa95a69ec1
Phillip Karamagi <pkaramagi1@gmail.com>
2025-03-10 13:33:40 +0200

    add embedder-type

--- src/docvector/core/embedders/huggingface_embedder.py
+++ src/docvector/core/embedders/huggingface_embedder.py
@@ -8,6 +8,8 @@ class HuggingFaceEmbedder(Embedder):
     def __init__(self, model_name:str = "sentence-transformers/all-MiniLM-L6-v2", device:str = None):
         self.device = device or "cpu"
         self.model = SentenceTransformer(model_name).to(self.device)
+        self.embedder_type = "huggingface"
+        
 
     def embed(self, text:str) -> List[float]:
         return self.model.encode(text, convert_to_nuppy=True).tolist()
--- src/docvector/core/embedders/openai_embedder.py
+++ src/docvector/core/embedders/openai_embedder.py
@@ -6,11 +6,14 @@ from core.config.settings import Settings
 class OpenAIEmbedder(Embedder):
     def __init__(self, model_name:str="text-embedding-3-small"):
         self.model_name = model_name
+        self.embedder_type = "openai"
 
     def embed(self, text:str) -> List[float]:
         settings = Settings()
         try:
+            
             openai_client = OpenAI(api_key=settings.openai.api_key)
+
             response = openai_client.embeddings.create(
                 model= self.model_name,
                 input= text
