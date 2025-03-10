e6bacd955d4e459267420bde4d06e815df95ae76
Phillip Karamagi <pkaramagi1@gmail.com>
2025-03-10 13:34:41 +0200

    feat: add chunk_text functionality for openai

--- src/docvector/core/text_splitters/base.py
+++ src/docvector/core/text_splitters/base.py
@@ -4,3 +4,7 @@ class TextSplitter(ABC):
     @abstractmethod
     def split(self, text:str) -> List[str]:
         pass
+
+    @abstractmethod
+    def chunk_text(self, text:str, token_limit:int, model:str)-> List[str]:
+        pass
