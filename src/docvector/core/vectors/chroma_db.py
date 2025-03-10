fea95cb2085f60ff748660c67c7d08fc3bc9dd0c
Phillip Karamagi <pkaramagi1@gmail.com>
2025-03-10 10:28:01 +0200

    feat: add search vector functionality

--- src/docvector/core/vectors/chroma_db.py
+++ src/docvector/core/vectors/chroma_db.py
@@ -9,7 +9,6 @@ class ChromaDB(VectorDB):
         self.collection_name = collection_name
         self.embedding_dim = embedding_dim
         self._create_collection()
-        
 
     @property
     def _doc(self) -> Dict[str, Any]:
@@ -20,6 +19,7 @@ class ChromaDB(VectorDB):
         }    
     
     def _create_collection(self) -> None:
+      
         collection = self.client.get_or_create_collection(self.collection_name)
         if collection is not None:
             print(f"Collection '{self.collection_name} is ready for use.")
@@ -39,11 +39,33 @@ class ChromaDB(VectorDB):
             ids.append(document_id)
             texts.append(vector.get("text",""))
             embeddings.append(vector.get("embedding",[]))
+            
+        
         
         collection.add(
             ids=ids,
             documents=texts,
-            embeddings=embeddings
+            embeddings=embeddings,
+           
         )
         print(f"Saved {len(vectors)} vectors to collection '{self.collection_name}'")
+
+    def search_vectors(self, query_vector:List[float], top_k:int = 3):
+
+        collection = self.client.get_collection(self.collection_name)
+        print(f"Total records in collection '{self.collection_name}': {collection.count()}")
+
+        results = collection.query(
+            query_embeddings=[query_vector],
+            n_results= top_k,
+        )
+        if results and "metadatas" in results and results["metadatas"]:
+            return results
+        return None
+    
+    
+    def remove_collection(self):
+        collection = self.client.get_collection(self.collection_name)
+        collection.delete(ids=None)
+        print(f"Collection {self.collection_name} has been reset")
         
